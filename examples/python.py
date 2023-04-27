72df046af3b6403acb485d7392d514643419fcc3
Zooce <zooce@hey.com>
2023-04-26 23:23:15 -0700

    generator rework - starting with python in vscode
    I'm going to try a slightly different approach to this whole thing:
    1. Each editor will have its own TOML file. The colors they reference
       will still be the ones defined in the main (now called palette.toml)
       config file. This way each editor's file can be more focused on that
       editor and it's quirks.
    2. The scopes for each color will now be as specific as possible and
       organized (simply by comments and blank lines) by language. This may
       sound crazy and against 'best practices' but these language scopes
       get crazy themselves and many language grammars don't always follow
       the suggested TextMate scopes anyways. With this I can keep the
       colors for each language separated and more precisely color the
       tokens.
    3. Realized/remembered that I can have many scopes in the "scope" key
       for VSCode so the generator and resulting color scheme are simplified
       a bit.

--- examples/python.py
+++ examples/python.py
@@ -1,7 +1,12 @@
 import os
 import numpy as np
 
-@requires_authorization
+MAX = 42
+
+def requires_authorization(name: str):
+    pass
+
+@requires_authorization("my function")
 def somefunc(param1='', param2=0):
     r'''A docstring'''
     if param1 > param2: # interesting
@@ -9,12 +14,16 @@ def somefunc(param1='', param2=0):
     return (param2 - param1 + 1) or None
 
 class SomeClass:
-    '''This is a docstring
+    '''This is a docstring.
     '''
+
+    def __init__(self):
+        self.pets = []
+
     def cats(arg):
         deg = [1, 2, '3', "4"]
         huh = deg.append(1)
-        return { "one": 1, "two": 2 }
+        return { "one": 1, "two": 2.0, "answer": MAX }
 
     def another(arg):  # oh really?
         pass
@@ -23,4 +32,7 @@ message = '''interpreter
 ... prompt'''
 
 if __name__ == "__main__":
-    print(f'this is {4 + 5}') # who doesn't love f-strings?
+    print(f'this is {4 + 5} and {6 * 7} and {12 % 34}') # who doesn't love f-strings?
+    cl = SomeClass()
+    c = cl.cats(arg=4)
+    sc = sorted(c.items(), key=lambda item: item[1], reverse=True)
--- generate.py
+++ generate.py
@@ -2,35 +2,34 @@ import json
 import toml
 
 
-def gen_sublime(config):
-    palette = config["palette"]
-    sublime = config["sublime"]
-    # todo
-    print("sublime not implemented yet")
+# def gen_sublime(config):
+#     palette = config["palette"]
+#     sublime = config["sublime"]
+#     # todo
+#     print("sublime not implemented yet")
 
-def gen_vim(config):
-    palette = config["palette"]
-    vim = config["vim"]
-    color_theme = [f"hi {hl_group} guifg={palette[color]} ctermfg={palette[color]}" for color,hl_groups in vim["colors"].items() for hl_group in hl_groups]
-    try:
-        with open(vim["file"]) as f:
-            f.writelines(color_theme)
-    except KeyError:
-        print("\n[[=== Vim ===]]")
-        print("\n".join(color_theme))
+# def gen_vim(config):
+#     palette = config["palette"]
+#     vim = config["vim"]
+#     color_theme = [f"hi {hl_group} guifg={palette[color]} ctermfg={palette[color]}" for color,hl_groups in vim["colors"].items() for hl_group in hl_groups]
+#     try:
+#         with open(vim["file"]) as f:
+#             f.writelines(color_theme)
+#     except KeyError:
+#         print("\n[[=== Vim ===]]")
+#         print("\n".join(color_theme))
 
-def gen_vs_code(config):
-    palette = config["palette"]
-    vs_code = config["vs_code"]
+def gen_vs_code(palette):
+    vscode = load_toml("vscode.toml")
     color_theme = {
         "$schema": "vscode://schemas/color-theme",
-        "name": vs_code["name"],
-        "type": vs_code["type"],
-        "colors": {scope:palette[color] for color,scopes in vs_code["colors"].items() for scope in scopes},
-        "tokenColors": [{ "scope": scope, "settings": { "foreground": palette[color] }} for color,scopes in vs_code["tokenColors"].items() for scope in scopes],
+        "name": vscode["name"],
+        "type": vscode["type"],
+        "colors": {scope:palette[color] for color,scopes in vscode["colors"].items() for scope in scopes},
+        "tokenColors": [{ "scope": scopes, "settings": { "foreground": palette[color] }} for color,scopes in vscode["tokenColors"].items()],
     }
     try:
-        with open(vs_code["file"], "w") as f:
+        with open(vscode["file"], "w") as f:
             json.dump(color_theme, f, indent=2)
     except KeyError:
         print("\n[[=== VS Code ===]]")
@@ -39,13 +38,13 @@ def gen_vs_code(config):
 
 def load_toml(file):
     with open(file) as f:
-        config = toml.load(f)
-    return config
+        toml_obj = toml.load(f)
+    return toml_obj
 
 
 if __name__ == "__main__":
-    config = load_toml("config.toml")
-    gen_sublime(config)
-    gen_vim(config)
-    gen_vs_code(config)
+    palette = load_toml("palette.toml")
+    # gen_sublime(palette)
+    # gen_vim(palette)
+    gen_vs_code(palette)
 
